e77482d3cf40d133a38fbf3d1d383144cbfe8282
bhlezzie <garbinbhlezzie@gmail.com>
2024-10-07 08:54:18 +0800

    Create agCheck.js

---
+++ agCheck.js
@@ -0,0 +1,8 @@
+let age = ("Enter your age:");
+
+if (age < 13) 
+    {console.log("You are a child.");} 
+  else if (age <= 19)
+    { console.log("You are a teenager.");} 
+  else (age <= 30)
+{console.log("You are an adult.");}
